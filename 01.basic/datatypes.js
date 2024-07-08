b6380aa3467e6f3ea07382b95040656508e94ccf
Kuntal-Manna <kuntal0208@gmail.com>
2024-07-08 19:44:31 +0000

    datatypes

---
+++ 01.basic/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Kuntal"
+let age = 28
+let isLoggedIn = false
+
+// number => 2 tp power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "28");
+console.log(typeof null); // object
