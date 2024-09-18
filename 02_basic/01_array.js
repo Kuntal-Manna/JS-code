611a041019e4e8584ea57a00ca08ab049edb4ac6
Kuntal Manna <kuntal0208@gmail.com>
2024-09-18 14:21:18 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,18 @@
+const myArr = [1, 2, 3, 4, 5]
+const myHeros = ["thor", "loki"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array Methods
+
+// myArr.push(6) add the data in last position of the array
+// myArr.push(7)
+// myArr.pop() delete the data from last position of the array
+
+// myArr.unshift(5) add the data in first position of the array
+// myArr.shift() delete the data from first position of the array
+
+
+
+console.log(myArr);
