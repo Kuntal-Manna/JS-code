3ddb4d3b99f0e25d0a6bb25855805fb6901421a3
Kuntal Manna <kuntal0208@gmail.com>
2024-09-18 15:27:09 +0000

    array practice

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -13,6 +13,26 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.unshift(5) add the data in first position of the array
 // myArr.shift() delete the data from first position of the array
 
+// console.log(myArr.includes(3)); 
+// console.log(myArr.indexOf(3));
 
+const newArr = myArr.join() 
+// convert the array to string
 
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+// console.log("A", myArr);
+// const myN1 = myArr.slice(1, 4)
+// console.log(myN1);
+// console.log(myArr);
+
+console.log("B", myArr);
+const myN2 = myArr.splice(1, 3)
+console.log(myN2);
 console.log(myArr);
+
+
+
